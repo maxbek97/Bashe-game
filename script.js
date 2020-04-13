3eb51b753308346593b6f83c08c37d6421984e89
[maxbek] <[maxbek294@gmail.com]>
2020-04-13 19:51:45 +0300

    panel js fixed

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 $(document).ready(function () {
     $('#switch-player-btn').on('click', function(){
-        $('#switch-player').html() == "1" ? $('#switch-player').html('2') : $('#switch-player').html('1');
-		$('#switch-player').html() == "1" ? $('#switch-player-btn').css("background-color", "#50e0ff") : $('#switch-player-btn').css("background-color", "#6cd883")
-    });
-})
+        if ($('#switch-player').html() == "1") {
+			$('#switch-player').html('2');
+			$('#switch-player-btn').css("background-color", "#50e0ff");
+		}
+		else {
+			$('#switch-player-btn').css("background-color", "#6cd883");
+			$('#switch-player').html('1');
+		}
+	});
+});
