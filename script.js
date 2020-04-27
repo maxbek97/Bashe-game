f697bb91ec0a6623a04bb409ab6fc94c77770c8f
[maxbek] <[maxbek294@gmail.com]>
2020-04-27 19:55:52 +0300

    finished site

--- script.js
+++ script.js
@@ -1,224 +1,27 @@
 $(document).ready(function () {
-    $('#switch-player-btn').on('click', function(){
-        if ($('#switch-player').html() == "1-й") {
-			$('#switch-player').html('2-й');
-			$('#switch-player-btn').css("background-color", "#6cd883");
-		}
-		else {
-			$('#switch-player-btn').css("background-color", "#50e0ff");
-			$('#switch-player').html('1-й');
-		}
-	});
-});
-$(document).ready(function () {
-    $('#1').on('click', function() {
-        if ($('#switch-player').html() == "1-й") {
-            $('#1').css('border-bottom',"2px solid #50e0ff");
+    var state = "state_1_js";
+    $('#switch-player-btn').on('click',	function(){
+        $(".chest-input").each(function(i, element){
+            $(element).attr('disabled', 'disabled');
+        });
+        var switchPlayer = $('#switch-player');
+        if (switchPlayer.html() == "1-й") {
+            $(switchPlayer).html('2-й');
+            $(this).toggleClass("state_1_js").toggleClass("state_2_js");
+            state = "state_2_js";
+        } else {
+            $(switchPlayer).html('1-й');
+            $(this).toggleClass("state_1_js").toggleClass("state_2_js");
+            state = "state_1_js";
         }
-        else {
-            $('#1').css("border-bottom", "2px solid #6cd883");
-        }
+        $(".chest-input").not(":checked").slice(0,4).removeAttr("disabled");
     });
+        $('.chest-input').change(function() {
+            if (this.checked) {
+                $(this).addClass(state);
+            }
+            else {
+                $(this).removeClass(state);
+            }
+        });
 });
-$(document).ready(function () {
-    $('#2').on('click', function() {
-        if ($('#switch-player').html() == "1-й") {
-            $('#2').css('border-bottom',"2px solid #50e0ff");
-        }
-        else {
-            $('#2').css("border-bottom", "2px solid #6cd883");
-        }
-    });
-});
-$(document).ready(function () {
-    $('#3').on('click', function() {
-        if ($('#switch-player').html() == "1-й") {
-            $('#3').css('border-bottom',"2px solid #50e0ff");
-        }
-        else {
-            $('#3').css("border-bottom", "2px solid #6cd883");
-        }
-    });
-});
-$(document).ready(function () {
-    $('#4').on('click', function() {
-        if ($('#switch-player').html() == "1-й") {
-            $('#4').css('border-bottom',"2px solid #50e0ff");
-        }
-        else {
-            $('#4').css("border-bottom", "2px solid #6cd883");
-        }
-    });
-});
-$(document).ready(function () {
-    $('#5').on('click', function() {
-        if ($('#switch-player').html() == "1-й") {
-            $('#5').css('border-bottom',"2px solid #50e0ff");
-        }
-        else {
-            $('#5').css("border-bottom", "2px solid #6cd883");
-        }
-    });
-});
-$(document).ready(function () {
-    $('#6').on('click', function() {
-        if ($('#switch-player').html() == "1-й") {
-            $('#6').css('border-bottom',"2px solid #50e0ff");
-        }
-        else {
-            $('#6').css("border-bottom", "2px solid #6cd883");
-        }
-    });
-});
-$(document).ready(function () {
-    $('#7').on('click', function() {
-        if ($('#switch-player').html() == "1-й") {
-            $('#7').css('border-bottom',"2px solid #50e0ff");
-        }
-        else {
-            $('#7').css("border-bottom", "2px solid #6cd883");
-        }
-    });
-});
-$(document).ready(function () {
-    $('#8').on('click', function() {
-        if ($('#switch-player').html() == "1-й") {
-            $('#8').css('border-bottom',"2px solid #50e0ff");
-        }
-        else {
-            $('#8').css("border-bottom", "2px solid #6cd883");
-        }
-    });
-});
-$(document).ready(function () {
-    $('#9').on('click', function() {
-        if ($('#switch-player').html() == "1-й") {
-            $('#9').css('border-bottom',"2px solid #50e0ff");
-        }
-        else {
-            $('#9').css("border-bottom", "2px solid #6cd883");
-        }
-    });
-});
-$(document).ready(function () {
-    $('#10').on('click', function() {
-        if ($('#switch-player').html() == "1-й") {
-            $('#10').css('border-bottom',"2px solid #50e0ff");
-        }
-        else {
-            $('#10').css("border-bottom", "2px solid #6cd883");
-        }
-    });
-});
-$(document).ready(function () {
-    $('#11').on('click', function() {
-        if ($('#switch-player').html() == "1-й") {
-            $('#11').css('border-bottom',"2px solid #50e0ff");
-        }
-        else {
-            $('#11').css("border-bottom", "2px solid #6cd883");
-        }
-    });
-});
-$(document).ready(function () {
-    $('#12').on('click', function() {
-        if ($('#switch-player').html() == "1-й") {
-            $('#12').css('border-bottom',"2px solid #50e0ff");
-        }
-        else {
-            $('#12').css("border-bottom", "2px solid #6cd883");
-        }
-    });
-});
-$(document).ready(function () {
-    $('#13').on('click', function() {
-        if ($('#switch-player').html() == "1-й") {
-            $('#13').css('border-bottom',"2px solid #50e0ff");
-        }
-        else {
-            $('#13').css("border-bottom", "2px solid #6cd883");
-        }
-    });
-});
-$(document).ready(function () {
-    $('#14').on('click', function() {
-        if ($('#switch-player').html() == "1-й") {
-            $('#14').css('border-bottom',"2px solid #50e0ff");
-        }
-        else {
-            $('#14').css("border-bottom", "2px solid #6cd883");
-        }
-    });
-});
-$(document).ready(function () {
-    $('#15').on('click', function() {
-        if ($('#switch-player').html() == "1-й") {
-            $('#15').css('border-bottom',"2px solid #50e0ff");
-        }
-        else {
-            $('#15').css("border-bottom", "2px solid #6cd883");
-        }
-    });
-});
-$(document).ready(function () {
-    $('#16').on('click', function() {
-        if ($('#switch-player').html() == "1-й") {
-            $('#16').css('border-bottom',"2px solid #50e0ff");
-        }
-        else {
-            $('#16').css("border-bottom", "2px solid #6cd883");
-        }
-    });
-});
-$(document).ready(function () {
-    $('#17').on('click', function() {
-        if ($('#switch-player').html() == "1-й") {
-            $('#17').css('border-bottom',"2px solid #50e0ff");
-        }
-        else {
-            $('#17').css("border-bottom", "2px solid #6cd883");
-        }
-    });
-});
-$(document).ready(function () {
-    $('#18').on('click', function() {
-        if ($('#switch-player').html() == "1-й") {
-            $('#18').css('border-bottom',"2px solid #50e0ff");
-        }
-        else {
-            $('#18').css("border-bottom", "2px solid #6cd883");
-        }
-    });
-});
-$(document).ready(function () {
-    $('#19').on('click', function() {
-        if ($('#switch-player').html() == "1-й") {
-            $('#19').css('border-bottom',"2px solid #50e0ff");
-        }
-        else {
-            $('#19').css("border-bottom", "2px solid #6cd883");
-        }
-    });
-});
-$(document).ready(function () {
-    $('#20').on('click', function() {
-        if ($('#switch-player').html() == "1-й") {
-            $('#20').css('border-bottom',"2px solid #50e0ff");
-        }
-        else {
-            $('#20').css("border-bottom", "2px solid #6cd883");
-        }
-    });
-});
-$(document).ready(function () {
-    $('#21').on('click', function() {
-        if ($('#switch-player').html() == "1-й") {
-            $('#21').css('border-bottom',"2px solid #50e0ff");
-        }
-        else {
-            $('#21').css("border-bottom", "2px solid #6cd883");
-        }
-    });
-});
-
-
