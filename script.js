72a264f6c16051fec1d8570063ba8a1086b9abb3
[maxbek] <[maxbek294@gmail.com]>
2020-05-06 12:37:55 +0300

    redact darkmod

--- script.js
+++ script.js
@@ -35,27 +35,6 @@ $(document).ready(function () {
 		location.reload();
 	});
 		$('.night-input').change(function() {
-			if (this.checked) {
-		$('body').css('background', '#37454c');
-		$('h1').css('background', '#37454c');
-		$('.container-view').css({'background-color' : '#506c8e', 'border' : '#223888 6px solid'});
-		$('.White-panel').css('background-color', '#757575');
-		$('.state_1_js').css('background-color', '#91c356');
-		$('.state_2_js').css('background-color', '#55c8d4');
-		$('.chest-input.state_1_js + .chest-text').css('border-bottom', '2px solid #91c356');
-		$('.chest-input.state_2_js + .chest-text').css('border-bottom', '2px solid #55c8d4');
-		$('.rulebox p').css('color', 'white');
-			}
-			else {
-		$('body').css('background', '#518ea0');
-		$('h1').css('background', '#518ea0');
-		$('.container-view').css({'background-color' : '#e8e2b3', 'border' : '#b9b388 6px solid'});
-		$('.White-panel').css('background-color', 'white');
-		$('.state_1_js').css('background-color', '#7eea00');
-		$('.state_2_js').css('background-color', '#21e0f3');
-		$('.chest-input.state_1_js + .chest-text').css('border-bottom', '2px solid #7eea00');
-		$('.chest-input.state_2_js + .chest-text').css('border-bottom', '2px solid #21e0f3');
-		$('.rulebox p').css('color', 'black');
-			}
+			$('body').toggleClass('darkmod');
 	});
 });
